ee2e81508e86a8303bd077b62b30a9bd8c357b58
NepoleonAsus <835410808@qq.com>
2018-07-18 20:34:54 +0800

    能部署

--- src/main/java/com/nuaa/health/HealthApplication.java
+++ src/main/java/com/nuaa/health/HealthApplication.java
@@ -2,10 +2,17 @@ package com.nuaa.health;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HealthApplication {
+public class HealthApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HealthApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HealthApplication.class, args);
 	}
