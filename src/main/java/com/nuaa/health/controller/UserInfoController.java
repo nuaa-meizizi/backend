53e94664d141584df68204d72452e8ee2f5af3d9
thelostin <849467474@qq.com>
2018-07-19 17:23:29 +0800

    将userid改成token

--- src/main/java/com/nuaa/health/controller/UserInfoController.java
+++ src/main/java/com/nuaa/health/controller/UserInfoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nuaa.health.service.UserInfoService;
+import com.nuaa.health.service.TokenService;
 import com.nuaa.health.util.GenericJsonResult;
 
 @RestController
@@ -16,19 +17,24 @@ import com.nuaa.health.util.GenericJsonResult;
 public class UserInfoController {
 	@Autowired
 	private UserInfoService userinfoService;
+	@Autowired
+	private TokenService tokenService;
 	@RequestMapping(value = "/getinfo",method=RequestMethod.GET)
-	public GenericJsonResult<Map<String, Object>> getinfo(@RequestParam(value = "userid", required = true) Long userid){
-		 return userinfoService.getinfo(userid);
+	public GenericJsonResult<Map<String, Object>> getinfo(@RequestParam(value = "token", required = true) String token){
+		Long userId = tokenService.getUid(token); 
+		return userinfoService.getinfo(userId);
     }
 
     @RequestMapping(value = "/updateinfo",method=RequestMethod.GET)
-	public GenericJsonResult<Map<String, Object>> updateinfo(@RequestParam(value = "userid", required = true) Long userid,@RequestParam(value = "province", required = true) String province,@RequestParam(value = "birthday", required = true) Long birthday){
-		 return userinfoService.updateinfo(userid,province,birthday);
+	public GenericJsonResult<Map<String, Object>> updateinfo(@RequestParam(value = "token", required = true) String token,@RequestParam(value = "province", required = true) String province,@RequestParam(value = "birthday", required = true) Long birthday){
+		Long userId = tokenService.getUid(token); 
+		return userinfoService.updateinfo(userId,province,birthday);
     }
 
     @RequestMapping(value = "/saveinfo",method=RequestMethod.GET)
-	public GenericJsonResult<Map<String, Object>> saveinfo(@RequestParam(value = "userid", required = true) Long userid,@RequestParam(value = "province", required = true) String province,@RequestParam(value = "birthday", required = true) Long birthday){
-		 return userinfoService.saveinfo(userid,province,birthday);
+	public GenericJsonResult<Map<String, Object>> saveinfo(@RequestParam(value = "token", required = true) String token,@RequestParam(value = "province", required = true) String province,@RequestParam(value = "birthday", required = true) Long birthday){
+		Long userId = tokenService.getUid(token); 
+		return userinfoService.saveinfo(userId,province,birthday);
 	}
 
 }
