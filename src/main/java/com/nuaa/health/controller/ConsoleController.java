cdaa2fa823c306a7be836716c8c4be631f038ed9
NepoleonAsus <835410808@qq.com>
2018-07-25 19:35:03 +0800

    设置模拟参数

--- src/main/java/com/nuaa/health/controller/ConsoleController.java
+++ src/main/java/com/nuaa/health/controller/ConsoleController.java
@@ -1,11 +1,14 @@
 package com.nuaa.health.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nuaa.health.entity.Simulation;
+import com.nuaa.health.repository.SimulationRepository;
 import com.nuaa.health.service.DeviceService;
 import com.nuaa.health.util.GenericJsonResult;
 import com.nuaa.health.util.HResult;
@@ -15,19 +18,33 @@ import com.nuaa.health.util.HResult;
 public class ConsoleController {
 	@Autowired
 	DeviceService deviceService;
-
+	@Autowired
+	SimulationRepository simulationRepository;
+	
 	@RequestMapping(value = "/iamlive", method = RequestMethod.GET)
 	public GenericJsonResult<String> iAmLive(@RequestParam(value = "imei", required = true) String imei) {
 		GenericJsonResult<String> res = new GenericJsonResult<>(HResult.S_OK);
 		deviceService.update(imei);
 		return res;
 	}
-
+	
 	@RequestMapping(value = "/livenum", method = RequestMethod.GET)
 	public GenericJsonResult<Integer> liveNum() {
 		GenericJsonResult<Integer> res = new GenericJsonResult<>(HResult.S_OK);
 		int count = deviceService.countLiveNum();
 		res.setData(count);
+		return res;
+	}
+	
+	@RequestMapping(value = "/setStatus", method = RequestMethod.POST)
+	public GenericJsonResult<String> fakeData(@RequestBody Simulation simulation) {
+		GenericJsonResult<String> res = new GenericJsonResult<>(HResult.S_OK);
+		try {
+			simulationRepository.save(simulation);
+		} catch (Exception e) {
+			res.setStatus(HResult.E_DATABASE_ERROR);
+			res.setData(e.toString());
+		}
 		return res;
 	}
 }
--- src/main/java/com/nuaa/health/entity/Simulation.java
+++ src/main/java/com/nuaa/health/entity/Simulation.java
@@ -23,61 +23,67 @@ public class Simulation {
 	private Integer sPressure;
 	@Column(name = "eye")
 	private Integer eye;
-
+	
 	public Long getUserId() {
 		return userId;
 	}
-
+	
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
-
+	
 	public Integer getTemperature() {
 		return temperature;
 	}
-
+	
 	public void setTemperature(Integer temperature) {
 		this.temperature = temperature;
 	}
-
+	
 	public Integer getHeartbeat() {
 		return heartbeat;
 	}
-
+	
 	public void setHeartbeat(Integer heartbeat) {
 		this.heartbeat = heartbeat;
 	}
-
+	
 	public Integer getWeight() {
 		return weight;
 	}
-
+	
 	public void setWeight(Integer weight) {
 		this.weight = weight;
 	}
-
+	
 	public Integer getdPressure() {
 		return dPressure;
 	}
-
+	
 	public void setdPressure(Integer dPressure) {
 		this.dPressure = dPressure;
 	}
-
+	
 	public Integer getsPressure() {
 		return sPressure;
 	}
-
+	
 	public void setsPressure(Integer sPressure) {
 		this.sPressure = sPressure;
 	}
-
+	
 	public Integer getEye() {
 		return eye;
 	}
-
+	
 	public void setEye(Integer eye) {
 		this.eye = eye;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "Simulation [userId=" + userId + ", temperature=" + temperature + ", heartbeat=" + heartbeat + ", weight=" + weight + ", dPressure=" + dPressure + ", sPressure="
+				+ sPressure + ", eye=" + eye + "]";
+	}
+	
 }
