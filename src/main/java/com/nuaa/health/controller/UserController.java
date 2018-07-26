1ebd232d3e02d0509dbefa5014f3b4e74b3d944c
NepoleonAsus <835410808@qq.com>
2018-07-26 20:07:11 +0800

    根据uid获取token

--- src/main/java/com/nuaa/health/controller/UserController.java
+++ src/main/java/com/nuaa/health/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.nuaa.health.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,26 +10,45 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nuaa.health.service.TokenService;
 import com.nuaa.health.service.UserService;
 import com.nuaa.health.util.GenericJsonResult;
+import com.nuaa.health.util.HResult;
 
 @RestController
 @RequestMapping(value = "user")
 public class UserController {
 	@Autowired
 	private UserService userService;
-	@RequestMapping(value = "/signup",method=RequestMethod.POST)
-	public GenericJsonResult<Map<String, Object>> signUp(@RequestParam(value = "name", required = true) String name,@RequestParam(value = "password", required = true) String password){
-		 return userService.signUp(name,password);
+	
+	@Autowired
+	private TokenService tokenService;
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public GenericJsonResult<Map<String, Object>> signUp(@RequestParam(value = "name", required = true) String name,
+			@RequestParam(value = "password", required = true) String password) {
+		return userService.signUp(name, password);
 	}
 	
-	@RequestMapping(value = "/login",method=RequestMethod.POST)
-	public GenericJsonResult<Map<String, Object>> login(@RequestParam(value = "name", required = true) String name,@RequestParam(value = "password", required = true) String password) {
-		return userService.login(name,password);
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public GenericJsonResult<Map<String, Object>> login(@RequestParam(value = "name", required = true) String name,
+			@RequestParam(value = "password", required = true) String password) {
+		return userService.login(name, password);
 	}
 	
-	@RequestMapping(value = "/getalluser",method=RequestMethod.GET)
+	@RequestMapping(value = "/getalluser", method = RequestMethod.GET)
 	public GenericJsonResult<ArrayList<Map<String, Object>>> getAllUser() {
 		return userService.getAllUser();
+	}
+	
+	@RequestMapping(value = "/getToken", method = RequestMethod.GET)
+	public GenericJsonResult<Map<String, Object>> getToken(@RequestParam(value = "uid", required = true) Long uid) {
+		GenericJsonResult<Map<String, Object>> result = new GenericJsonResult<Map<String, Object>>(HResult.S_OK);
+		String token = tokenService.updateToken(uid);
+		Map<String, Object> data = new HashMap<String, Object>();  
+		data.put("id", uid);
+		data.put("token", token);
+		result.setData(data);
+		return result;
 	}
 }
